21378c9d02880010398947ac6d71f88aee29f806
ShristiA <shristiamatya3@gmail.com>
2018-10-05 16:58:20 -0400

    new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
+
 package com.company;
-import java.util.Scanner;
+        import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -7,36 +8,45 @@ public class Main {
         int num;
         int seven = 0;
         int counter = 1;
-
-
-
         String response;
-        System.out.println("Enter a number");
-        num = src.nextInt();
 
-        while(num>=0){
-
-            System.out.println(num);
-            seven = num / 7;
-            System.out.println(seven * 7);
-            num =  num - counter * 5;
-            counter++;
+        // creating loop
+        do {
+            System.out.println("Enter a number");
+            num = src.nextInt();
+            for (int i = num; i <= num; i--) {
+                myMethod(i);
+                //calling method so that it can run the codes method under it.
             }
-                    System.out.println("Would you like to enter another number(y/n)");
-                    response = src.next();
-                    if (response.equals("y")) {
-                        System.out.println("Enter a number");
-                        num = src.nextInt();
-                    } else
-                        System.out.println("Thanks for Playing!");
-                }
+            System.out.println("Would you like to enter another number(y/n)");
+            response = src.next();
+            System.out.println("Would you like to enter anthoer number (y/n)");
+            response = src.next();
 
-
-
-
-
-
-
-
+            // asking and giving the output
+        } while (!response.equalsIgnoreCase("n"));
+        //condition to end while loop.
+    }
+    public static void myMethod(int num){
+        if(num>0) {
+            if (num % 5 == 0 || num % 7 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
 
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
